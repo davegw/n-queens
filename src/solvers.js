b34ae2a1617b866867315b061ec534427d1137e6
Hack Reactor Students <dgertmenianwong@gmail.com>
2014-06-12 10:27:20 -0700

    Replace call to board rows with variable lookup

--- src/solvers.js
+++ src/solvers.js
@@ -14,6 +14,7 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
+  var a = new Date();
   var solutions = [];
   var initialBoard = new Board({n: n});
   var solveBoard = function(board, numPieces, usedCols) {
@@ -23,7 +24,7 @@ window.findNRooksSolution = function(n) {
       return;
     }
     for (var row = numPieces; row < numPieces + 1; row++) {
-      for (var col = 0; col < board.rows().length; col++) {
+      for (var col = 0; col < n; col++) {
         if (board.rows()[row][col] !== 1 && usedCols[col] === undefined) {
           // Create a copy of the current board to avoid mutation.
           var copyBoard = _.map(board.rows(), function(item){
@@ -40,7 +41,7 @@ window.findNRooksSolution = function(n) {
   };
   solveBoard(initialBoard, 0, {});
   var solution = solutions[0] || null;
-
+  console.log((new Date() - a)/1000);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
