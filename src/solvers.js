5125503157d223c61cee509e53cddf8979f83a4a
Hack Reactor Students <dgertmenianwong@gmail.com>
2014-06-11 20:25:11 -0700

    Completed board methods and find rook solver.

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,31 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solutions = [];
+  var initialBoard = new Board({n: n});
+  var solveBoard = function(board, numPieces) {
+    if (numPieces === n) {
+      if (!board.hasAnyRooksConflicts()) {
+        solutions.push(board.rows());
+      }
+    } else {
+      for (var row = 0; row < board.rows().length; row++) {
+        for (var col = 0; col < board.rows().length; col++) {
+          if (board.rows()[row][col] !== 1) {
+            var copyBoard = _.map(board.rows(), function(item){
+              return item.slice();
+            });
+            var newBoard = new Board(copyBoard);
+            newBoard.togglePiece(row, col);
+            solveBoard(newBoard, numPieces + 1);
+          }
+        }
+      }
+
+    }
+  };
+  solveBoard(initialBoard, 0);
+  var solution = solutions[0] || null;
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
